a76a9eee8f9cc612ac6a1b8f24972de72fb7865d
stanzhai <stanzhai@outlook.com>
2017-02-28 00:44:36 +0800

    add global hotkey

--- GoldenPassport/AppDelegate.swift
+++ GoldenPassport/AppDelegate.swift
@@ -7,16 +7,54 @@
 //
 
 import Cocoa
+import Carbon
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private var monitor: Any?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-
+        let opts = NSDictionary(object: kCFBooleanTrue, forKey: kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString) as CFDictionary
+        guard AXIsProcessTrustedWithOptions(opts) == true else { return }
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown, handler: self.handleKeydownEvent)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        if monitor != nil {
+            NSEvent.removeMonitor(monitor!)
+            monitor = nil
+        }
+    }
+    
+    func handleKeydownEvent(_ event: NSEvent) {
+        let flag = event.modifierFlags.rawValue
+        // flag of: Shift + Cmd
+        if flag != 1179924 && flag != 1179914 {
+            return
+        }
+        
+        if let char = event.characters {
+            if let num = Int(char) {
+                copyAuthCode(idx: num)
+            }
+        }
+    }
+    
+    private func copyAuthCode(idx: Int) {
+        let authCodes = DataManager.shared.allAuthCode()
+        if (idx >= authCodes.count) {
+            return
+        }
+        var i = 0
+        for codeInfo in authCodes {
+            if i == idx {
+                let pasteboard = NSPasteboard.general()
+                pasteboard.clearContents()
+                pasteboard.setString(codeInfo.value, forType: NSStringPboardType)
+                break
+            }
+            i = i + 1
+        }
     }
 }
 
