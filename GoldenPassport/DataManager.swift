09fdb5638a03c97193aa3537bb9f7c8dc4b113b2
Mac Chow <vifix.mac@gmail.com>
2017-04-26 19:19:21 +0800

    加入了控制HTTP服务器的开关

--- GoldenPassport/DataManager.swift
+++ GoldenPassport/DataManager.swift
@@ -12,95 +12,100 @@ final class DataManager {
     static let shared = DataManager()
     private let authDataFile = "gp.secrets"
     private let configFile = "config.plist"
-    
-    private var authData: [String : String]
-    private var config: [String : String]
-    
+
+    private var authData: [String: String]
+    private var config: [String: String]
+
     private init() {
         authData = [:]
         config = [:]
         authData = loadData(authDataFile)
         config = loadData(configFile)
     }
-    
+
     func addOTPAuthURL(tag: String, url: String) {
         authData[tag] = url
         saveData(authDataFile, data: authData)
     }
-    
+
     func removeOTPAuthURL(tag: String) {
         authData.removeValue(forKey: tag)
         saveData(authDataFile, data: authData)
     }
-    
-    func allAuthCode() -> [String : String] {
-        var result: [String : String] = [:]
+
+    func allAuthCode() -> [String: String] {
+        var result: [String: String] = [:]
         for d in authData {
             let url = d.value
             let otpData = OTPAuthURLParser(url)!
-            
+
             let data = OTPAuthURL.base32Decode(otpData.secret)
             let gen = TOTPGenerator(secret: data,
-                                    algorithm: TOTPGenerator.defaultAlgorithm(),
-                                    digits: TOTPGenerator.defaultDigits(),
-                                    period: TOTPGenerator.defaultPeriod())
+                    algorithm: TOTPGenerator.defaultAlgorithm(),
+                    digits: TOTPGenerator.defaultDigits(),
+                    period: TOTPGenerator.defaultPeriod())
             let code = gen?.generateOTP(for: Date())
-            
+
             result[d.key] = code
         }
         return result
     }
-    
+
     func dataCount() -> Int {
         return authData.count
     }
 
     func getHttpServerAutoStart() -> Bool {
-        return getConfig("http_server_auto_start") ?? true
+        let auto = getConfig("http_server_auto_start")
+        if (auto == nil || auto == "true") {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     func saveHttpServerAutoStart(auto: Bool) {
-        saveConfig(key: "http_server_auto_start", value: auto)
+        saveConfig(key: "http_server_auto_start", value: "\(auto)")
     }
-    
+
     func getHttpServerPort() -> String {
         return getConfig("http_server_port") ?? "\(DEFAULT_HTTP_PORT)"
     }
-    
+
     func saveHttpServerPort(port: String) {
         saveConfig(key: "http_server_port", value: port)
     }
-    
+
     func getConfig(_ key: String) -> String? {
         return config[key]
     }
-    
+
     func saveConfig(key: String, value: String) {
         config[key] = value
         saveData(configFile, data: config)
     }
-    
-    private func saveData(_ dataFile: String, data: [String : String]) {
+
+    private func saveData(_ dataFile: String, data: [String: String]) {
         let fileLocation = "\(dataFilePath)\(dataFile)"
         let fileUrl = URL(fileURLWithPath: fileLocation)
         try? NSKeyedArchiver.archivedData(withRootObject: data).write(to: fileUrl)
     }
-    
-    private func loadData(_ dataFile: String) -> [String : String] {
+
+    private func loadData(_ dataFile: String) -> [String: String] {
         let fileLocation = "\(dataFilePath)\(dataFile)"
         let d = NSKeyedUnarchiver.unarchiveObject(withFile: fileLocation)
         if d != nil {
-            return d as! [String : String]
+            return d as! [String: String]
         }
         return [:]
     }
-    
+
     private var dataFilePath: String {
         let fileManager = FileManager.default
         let path = NSSearchPathForDirectoriesInDomains(
-            Foundation.FileManager.SearchPathDirectory.applicationSupportDirectory,
-            Foundation.FileManager.SearchPathDomainMask.userDomainMask,
-            true).first! + "/GoldenPassport/"
+                Foundation.FileManager.SearchPathDirectory.applicationSupportDirectory,
+                Foundation.FileManager.SearchPathDomainMask.userDomainMask,
+                true).first! + "/GoldenPassport/"
         if !fileManager.fileExists(atPath: path) {
             do {
                 try fileManager.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
@@ -108,7 +113,7 @@ final class DataManager {
                 print(error)
             }
         }
-        
+
         return path
     }
 }
